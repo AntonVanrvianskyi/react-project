4bc79b786da6c016ba1ca96a8dd02c7f6666e020
AntonVarvianskyi <anton.varvianskyi@gmail.com>
2023-04-26 20:41:15 +0300

    hw5

--- src/components/form/DogForm.js
+++ src/components/form/DogForm.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import {useForm} from "react-hook-form";
+import Dog from "./Dog";
+import {MyContext} from "./Form";
+const reducer = (state, action) => {
+  switch (action.type){
+      case 'dog':
+          return{
+              ...state,
+              dogs:[...state.dogs, action.payload]
+          }
+  }
+}
+const DogForm = () => {
 
-const DogForm = ({dispatch}) => {
+    const [state, dispatch] = useReducer(reducer, {dogs:[]})
     const {register,handleSubmit, reset} = useForm()
     const saveDog = (dog) => {
         dispatch({type:'dog', payload:dog.dog})
         reset()
     }
     return (
+        <div>
         <form onSubmit={handleSubmit(saveDog)}>
             <label>Add Dog:  </label>
             <input placeholder={'dog'} {...register('dog')}/>
             <button>Save</button>
         </form>
+            <div>
+                {state.dogs.map((dog,index)=><Dog key={index} dog={dog}/>)}
+            </div>
+
+        </div>
     );
 };
 
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,12 @@
-import React, {useReducer} from 'react';
+import React, {createContext, useReducer} from 'react';
 import {useForm} from "react-hook-form";
 import Cat from "./Cat";
 import './DogFom.css'
 import Dog from "./Dog";
+import DogForm from "./DogForm";
+import {defaults} from "axios";
 
-
+const MyContext = createContext('default')
 const reducer = (state,action) => {
     switch (action.type) {
         case 'cat':
@@ -12,11 +14,7 @@ const reducer = (state,action) => {
                 ...state,
                 cats: [...state.cats, action.payload]
             }
-        case 'dog':
-            return {
-                ...state,
-                dogs:[...state.dogs, action.payload]
-            }
+
 
 
     }
@@ -35,37 +33,26 @@ const Form = () => {
         reset()
 
     }
-    const saveDog = (dog) => {
-        dispatch({type:'dog', payload:dog.dog})
-        reset()
-    }
-
-
 
     return (
+
         <div className={'dog-form'}>
                 <form className={'cat-form'} onSubmit={handleSubmit(save)}>
                     <label>Add Cat:  </label>
                     <input placeholder={'cat'} {...register('name')}/>
                     <button>Save</button>
                 </form>
-            <div>
-                <form onSubmit={handleSubmit(saveDog)}>
-                    <label>Add Dog:  </label>
-                    <input placeholder={'dog'} {...register('dog')}/>
-                    <button>Save</button>
-                </form>
-            </div>
 
-            <div className={'animal'}>
+            <DogForm/>
+
+
             {state.cats.map((cat,index)=><Cat key={index} cat={cat}/>)}
-            {state.dogs.map((dog,index)=><Dog key={index} dog={dog}/>)}
-            </div>
 
 
         </div>
 
+
     );
 };
 
-export default Form;
+export default Form
